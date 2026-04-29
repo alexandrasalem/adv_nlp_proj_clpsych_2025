351feec45318bfb0745f8c4cb8fcf41e0eb580b4
Alex Salem <alexandrasalem8@gmail.com>
2026-04-29 19:46:15 -0400

    debugging

--- running_task_b_baselines.py
+++ running_task_b_baselines.py
@@ -17,24 +17,23 @@ post_csv_location = args.post_csv_location
 
 # Baseline from BLUE team
 def single_blue_team_post_summary(post_text, model, tokenizer, device):
-    prompt = f"Given the following Reddit post, summarize the interplay between adaptive and maladaptive self-states."
-    messages = [
-        {"role": "system", "content": prompt},
-        {"role": "user", "content": post_text},
-    ]
+    prompt = f"""
+    Given the following Reddit post, summarize the interplay between adaptive and maladaptive self-states.
 
-    inputs = tokenizer.apply_chat_template(
-        messages,
-        add_generation_prompt=True,
-        return_tensors="pt"
-    )
+    Post:
+    \"{post_text}\"
+
+    Response format:
+    {{ "summary": "<post-level summary>" }}
+    """
+
+    inputs = tokenizer(prompt, return_tensors="pt").to(device)
 
     outputs = model.generate(**inputs,
                              temperature=0.7,
                              top_p=0.9,
                              do_sample=True,
                              max_new_tokens=128)
-
     generated_tokens = outputs[0][inputs["input_ids"].shape[-1]:]
     decoded_output = tokenizer.decode(generated_tokens, skip_special_tokens=True)
     print("here")
@@ -56,8 +55,9 @@ def main():
     tokenizer.pad_token = tokenizer.eos_token
     model = AutoModelForCausalLM.from_pretrained(
         "meta-llama/Llama-3.2-3B",
-        device_map="auto",
     )
+
+    model.to(device)
 
     # running on post
     output = single_blue_team_post_summary(post_text = post_test, model = model, tokenizer = tokenizer, device = device)
